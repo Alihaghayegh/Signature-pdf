6ae08e04ccecdd902104f7bc843f36f0e5ff9853
alihaghayegh <haghayeghali95@gmail.com>
2024-06-12 10:40:57 +0330

    4: Some comments added to serializers

--- personal_info/serializers.py
+++ personal_info/serializers.py
@@ -3,6 +3,9 @@ from rest_framework import serializers
 
 
 class UserSerializerForDB(serializers.Serializer):
+    '''
+    This class is for getting user info and credetials for saving info database
+    '''
     class Meta:
         model = UserInfo
         fields = ['username', 'password', 'first_name', 'last_name', 'signature', 'time']
@@ -10,6 +13,9 @@ class UserSerializerForDB(serializers.Serializer):
 
 
 class UserSerializerForResponse(serializers.Serializer):
+    '''
+    This class is for returning user info and credentials in response
+    '''
     class Meta:
         model = UserInfo
         fields = ['first_name', 'last_name', 'signature', 'time']
